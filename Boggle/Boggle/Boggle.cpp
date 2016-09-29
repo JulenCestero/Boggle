269f618dbdef5c59cb2ba0240df0b806f70864db
Ane <ane.berasategi@gmail.com>
2016-09-29 20:07:23 +0200

    Ane entiende el Trie

--- Boggle/Boggle/Boggle.cpp
+++ Boggle/Boggle/Boggle.cpp
@@ -2,10 +2,31 @@
 //
 
 #include "stdafx.h"
-
+#include "Classes.h"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	Trie* trie = new Trie();
+  trie->addWord("Hello");
+  trie->addWord("Balloon");
+  trie->addWord("Ball");
+
+  if (trie->searchWord("Hell"))
+     cout << "Found Hell" << endl;
+
+  if (trie->searchWord("Hello"))
+      cout << "Found Hello" << endl;
+
+  if (trie->searchWord("Helloo"))
+      cout << "Found Helloo" << endl;
+
+  if (trie->searchWord("Ball"))
+      cout << "Found Ball" << endl;
+
+  if (trie->searchWord("Balloon"))
+      cout << "Found Balloon" << endl;
+
+  delete trie;
 	return 0;
 }
 
--- Boggle/Boggle/Classes.h
+++ Boggle/Boggle/Classes.h
@@ -4,13 +4,13 @@ class Node {
 public:
     Node() { mContent = ' '; mMarker = false; }
     ~Node() {}
-    char content() { return mContent; }
-    void setContent(char c) { mContent = c; }
-    bool wordMarker() { return mMarker; }
-    void setWordMarker() { mMarker = true; }
+    char content() {return mContent;}
+    void setContent(char c) {mContent = c;}
+    bool wordMarker() {return mMarker;}
+    void setWordMarker() {mMarker = true;}
     Node* findChild(char c);
-    void appendChild(Node* child) { mChildren.push_back(child); }
-    vector<Node*> children() { return mChildren; }
+    void appendChild(Node* child) {mChildren.push_back(child);}
+    vector<Node*> children() {return mChildren;}
 
 private:
     char mContent;
